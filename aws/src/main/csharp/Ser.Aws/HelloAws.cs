bee3edd4672e06892a0338f368c991bfbf1df9d7
Vince Iborra <vince.iborra@gmail.com>
2018-05-24 21:14:52 +1000

    Have renamed solution, project names and have set namespaces to something a bit more reasonable.

---
+++ aws/src/main/csharp/Ser.Aws/HelloAws.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+using Amazon;
+using Amazon.EC2;
+using Amazon.EC2.Model;
+using Amazon.SimpleDB;
+using Amazon.SimpleDB.Model;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+
+namespace Vji.Bob {
+
+    class HelloAws {
+
+        public string GetServiceOutput() {
+
+            //var credentials = new StoredProfileAWSCredentials("vjisys");
+            var chain = new CredentialProfileStoreChain();
+            CredentialProfile basicProfile;
+            AWSCredentials credentials = null;
+            if (chain.TryGetProfile("default", out basicProfile)) {
+                // Use basicProfile
+                credentials = basicProfile.GetAWSCredentials(null);
+            }
+
+            StringBuilder sb = new StringBuilder(1024);
+            using (StringWriter sr = new StringWriter(sb)) {
+                sr.WriteLine("===========================================");
+                sr.WriteLine("Welcome to the AWS .NET SDK!");
+                sr.WriteLine("===========================================");
+
+                // Print the number of Amazon EC2 instances.
+                //IAmazonEC2 ec2 = new AmazonEC2Client();
+                IAmazonEC2 ec2 = new AmazonEC2Client(credentials, RegionEndpoint.APSoutheast2);
+                DescribeInstancesRequest ec2Request = new DescribeInstancesRequest();
+
+                try {
+                    DescribeInstancesResponse ec2Response = ec2.DescribeInstances(ec2Request);
+                    int numInstances = 0;
+                    numInstances = ec2Response.Reservations.Count;
+                    sr.WriteLine(string.Format("You have {0} Amazon EC2 instance(s) running in the {1} region.",
+                                               numInstances, ConfigurationManager.AppSettings["AWSRegion"]));
+                }
+                catch (AmazonEC2Exception ex) {
+                    if (ex.ErrorCode != null && ex.ErrorCode.Equals("AuthFailure")) {
+                        sr.WriteLine("The account you are using is not signed up for Amazon EC2.");
+                        sr.WriteLine("You can sign up for Amazon EC2 at http://aws.amazon.com/ec2");
+                    }
+                    else {
+                        sr.WriteLine("Caught Exception: " + ex.Message);
+                        sr.WriteLine("Response Status Code: " + ex.StatusCode);
+                        sr.WriteLine("Error Code: " + ex.ErrorCode);
+                        sr.WriteLine("Error Type: " + ex.ErrorType);
+                        sr.WriteLine("Request ID: " + ex.RequestId);
+                    }
+                }
+                sr.WriteLine();
+
+
+                // vjivjivji
+                //var vpcresponse = ec2.DescribeVpcs(new DescribeVpcsRequest {
+                //    VpcIds = new List<string> {
+                //    "vpc-ead45e8d "
+                //    }
+                //});
+                var vpcresponse = ec2.DescribeVpcs();
+
+                List<Vpc> vpcs = vpcresponse.Vpcs;
+                for (var idx = 0; idx < vpcs.Count; idx++) {
+                    Vpc vpc = vpcs[idx];
+                    sr.WriteLine("VPC: " + vpc.ToString());
+                    List<Amazon.EC2.Model.Tag> tags = vpc.Tags;
+                    for (var jdx = 0; jdx < tags.Count; jdx++) {
+                        Amazon.EC2.Model.Tag tag = tags[jdx];
+                        sr.WriteLine("Key: " + tag.Key + ", Value: " + tag.Value);
+                    }
+                }
+                sr.WriteLine();
+                // vjivjivji
+
+
+
+                // Print the number of Amazon SimpleDB domains.
+                //IAmazonSimpleDB sdb = new AmazonSimpleDBClient();
+                IAmazonSimpleDB sdb = new AmazonSimpleDBClient(credentials, RegionEndpoint.APSoutheast2);
+                ListDomainsRequest sdbRequest = new ListDomainsRequest();
+
+                try {
+                    ListDomainsResponse sdbResponse = sdb.ListDomains(sdbRequest);
+
+                    int numDomains = 0;
+                    numDomains = sdbResponse.DomainNames.Count;
+                    sr.WriteLine(string.Format("You have {0} Amazon SimpleDB domain(s) in the {1} region.",
+                                               numDomains, ConfigurationManager.AppSettings["AWSRegion"]));
+                }
+                catch (AmazonSimpleDBException ex) {
+                    if (ex.ErrorCode != null && ex.ErrorCode.Equals("AuthFailure")) {
+                        sr.WriteLine("The account you are using is not signed up for Amazon SimpleDB.");
+                        sr.WriteLine("You can sign up for Amazon SimpleDB at http://aws.amazon.com/simpledb");
+                    }
+                    else {
+                        sr.WriteLine("Caught Exception: " + ex.Message);
+                        sr.WriteLine("Response Status Code: " + ex.StatusCode);
+                        sr.WriteLine("Error Code: " + ex.ErrorCode);
+                        sr.WriteLine("Error Type: " + ex.ErrorType);
+                        sr.WriteLine("Request ID: " + ex.RequestId);
+                    }
+                }
+                sr.WriteLine();
+
+                // Print the number of Amazon S3 Buckets.
+                //IAmazonS3 s3Client = new AmazonS3Client();
+                IAmazonS3 s3Client = new AmazonS3Client(credentials, RegionEndpoint.APSoutheast2);
+
+                try {
+                    ListBucketsResponse response = s3Client.ListBuckets();
+                    int numBuckets = 0;
+                    if (response.Buckets != null &&
+                        response.Buckets.Count > 0) {
+                        numBuckets = response.Buckets.Count;
+                    }
+                    sr.WriteLine("You have " + numBuckets + " Amazon S3 bucket(s).");
+                }
+                catch (AmazonS3Exception ex) {
+                    if (ex.ErrorCode != null && (ex.ErrorCode.Equals("InvalidAccessKeyId") ||
+                        ex.ErrorCode.Equals("InvalidSecurity"))) {
+                        sr.WriteLine("Please check the provided AWS Credentials.");
+                        sr.WriteLine("If you haven't signed up for Amazon S3, please visit http://aws.amazon.com/s3");
+                    }
+                    else {
+                        sr.WriteLine("Caught Exception: " + ex.Message);
+                        sr.WriteLine("Response Status Code: " + ex.StatusCode);
+                        sr.WriteLine("Error Code: " + ex.ErrorCode);
+                        sr.WriteLine("Request ID: " + ex.RequestId);
+                    }
+                }
+                sr.WriteLine("Press any key to continue...");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ aws/src/main/csharp/Ser.Aws/Main.cs
@@ -0,0 +1,199 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: vji
+ * Date: 17/12/2017
+ * Time: 2:48 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+
+using Spring.Context;
+using Spring.Context.Support;
+using Vji.Bob;
+
+namespace Ser.Aws {
+
+	public class Main {
+
+		public string filecontents = "";
+		public bool readline = false;
+		private bool m_ShowFullMenus = false;
+		public Form1 theForm;
+
+        private IApplicationContext _context = null;
+
+        public IApplicationContext context {
+            get {
+                if (this._context == null) {
+                    this._context = new XmlApplicationContext("assembly://Ser.Aws/Ser.Aws/context.xml");
+                }
+                return this._context;
+            }
+        }
+
+		//Called Before EA starts to check Add-In Exists
+		public String EA_Connect(EA.Repository Repository) {
+			//No special processing required.
+			return "a string";
+		}
+		
+		public object ProcessOperation(EA.Repository repository, object array) {
+			string elementGuid = ((string[]) array)[0];
+			EA.Project project = repository.GetProjectInterface();
+			bool ret = project.GenerateClass (elementGuid, "overwrite=1");
+			return "hello" + ret.ToString();
+		}
+		
+		public bool EA_OnPostNewConnector(EA.Repository repository, EA.EventProperties info) {
+			int count = info.Count;
+			EA.EventProperty property = info.Get(0);
+			string str = (string) property.Value;
+			int connectorId = Convert.ToInt32(str);
+			EA.Connector connector = repository.GetConnectorByID(connectorId);
+			connector.Name = "vji";
+
+            HelloAws aws = new HelloAws();
+            string notes = aws.GetServiceOutput();
+            connector.Notes = notes;
+
+            connector.Update();
+            
+			return true;
+		}
+
+		//Called when user Click Add-Ins Menu item from within EA.
+		//Populates the Menu with our desired selections.
+		public object EA_GetMenuItems(EA.Repository Repository, string Location, string MenuName) {
+			EA.Package aPackage = Repository.GetTreeSelectedPackage();
+			switch( MenuName )
+			{
+				case "":
+					return "-&AWSS";
+				case "-&AWSS":
+					string[] ar = { "&Tagged CSV Export", "Menu2", "About..." };
+					return ar;
+			}
+			return "";
+		}
+		
+		//Sets the state of the menu depending if there is an active project or not
+		bool IsProjectOpen(EA.Repository Repository)
+		{
+			try
+			{
+				EA.Collection c = Repository.Models;
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		//Called once Menu has been opened to see what menu items are active.
+		public void EA_GetMenuState(EA.Repository Repository, string Location, string MenuName, string ItemName, ref bool IsEnabled, ref bool IsChecked)
+		{
+			if( IsProjectOpen(Repository) )
+			{
+                if (ItemName == "&Tagged CSV Export")
+                    IsChecked = m_ShowFullMenus;
+                else if (ItemName == "Menu2") {
+                    IsChecked = m_ShowFullMenus;
+                    //IsEnabled = m_ShowFullMenus;
+                }
+			}
+			else
+				// If no open project, disable all menu options
+				IsEnabled = false;
+		}
+
+		//Called when user makes a selection in the menu.
+		//This is your main exit point to the rest of your Add-in
+		public void EA_MenuClick(EA.Repository Repository, string Location, string MenuName, string ItemName)
+		{						
+			switch( ItemName )
+			{
+				case "&Tagged CSV Export":	
+
+					String writerString;
+					String tagString;
+
+					tagString = "";
+					writerString = "";
+
+					EA.Package aPackage;
+					aPackage = Repository.GetTreeSelectedPackage();
+
+					foreach(EA.Package thePackage in aPackage.Packages)
+					{
+						writerString = writerString + thePackage.Name.ToString() + "," + thePackage.ObjectType.ToString() + "\n";
+						//MessageBox.Show(writerString);
+						foreach(EA.Element theElements in thePackage.Elements)
+						{
+							foreach(EA.TaggedValue theTags in theElements.TaggedValues)
+							{	
+								tagString = tagString + theTags.Name.ToString() + "," + theTags.Value.ToString() + ",";
+							}
+							writerString = writerString + theElements.Name.ToString() + "," + theElements.ObjectType.ToString() + "," + tagString + "\n";
+							tagString = "";
+						}
+					}
+
+					foreach(EA.Element theElements in aPackage.Elements)
+					{
+						foreach(EA.TaggedValue theTags in theElements.TaggedValues)
+						{	
+							tagString = tagString + theTags.Name.ToString() + "," + theTags.Value.ToString() + ",";
+						}
+						writerString = writerString + theElements.Name.ToString() + "," + theElements.ObjectType.ToString() + "," + tagString;
+						tagString = "";
+					}
+
+					
+					// create a writer and open the file
+					TextWriter tw = new StreamWriter("c:\\vji\\tmp\\" + aPackage.Name.ToString() + ".csv");
+
+					// write a line of text to the file
+					tw.WriteLine(writerString);
+
+					// close the stream
+					tw.Close();
+
+					break;					
+
+				case "Menu2":
+                    IApplicationContext ctx = this.context;
+					break;
+
+				case "About...":
+					Form1 anAbout = new Form1();
+					anAbout.ShowDialog();					
+					break;
+			}
+		}
+		public bool readLine(StreamReader sreader)
+		{
+			bool answer;
+			answer = false;
+			
+			try
+			{
+				filecontents = filecontents + "\n" + sreader.ReadLine();
+			}
+			catch
+			{
+				answer = true;
+			}
+
+			return answer;
+		}
+	}
+}
+		
+		
+	
+
