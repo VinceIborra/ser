4d5f6d2a81e91e41c1d30ca871db6de8c8d89186
Vince Iborra <vince.iborra@gmail.com>
2018-07-06 21:09:45 +1000

    Issue000011:  Factored out model creation into AwsModeller

---
+++ src/main/csharp/Ser.Ea.Addin.Aws/IAwsModelCache.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EA;
+
+namespace Ser.Ea.Addin.Aws {
+    interface IAwsModelCache {
+        void Add(string id, Element element);
+    }
+}
